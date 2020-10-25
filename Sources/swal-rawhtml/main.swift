28fca51197925e0db0ea4ab19bb04dc974259451
John Garrett <jacgarrett18@gmail.com>
2020-10-25 15:40:50 -0400

    build out design

--- Sources/swal-rawhtml/main.swift
+++ Sources/swal-rawhtml/main.swift
@@ -6,48 +6,64 @@ func element(_ id: String) -> JSObject? {
 }
 
 // combinations
-var cInput = [1, 2, 3, 4]
-let combinations = { (value: JSValue?) -> JSValue in
-    guard let value = value, let stringValue = value.string, let count = Int(stringValue) else {
-        return "Error parsing input".jsValue()
-    }
-    return cInput.combinations(ofCount: count).map { $0.description }.joined(separator: "<br>").jsValue()
+var combInput = [1, 2, 3, 4] {
+    didSet { renderCombinations() }
+}
+var combCount = 1 {
+    didSet { renderCombinations() }
 }
 
-element("combo-array-input")?.value = cInput.description.jsValue()
-element("combo-output")?.innerHTML = combinations(element("combo-count-input")?.value)
+func renderCombinations() {
+    element("combo-output")?.innerHTML = combInput
+        .combinations(ofCount: combCount)
+        .map { $0.description }
+        .joined(separator: "<br>")
+        .jsValue()
+    element("combo-array-input")?.value = combInput.description.jsValue()
+    element("combo-count-input")?.value = combCount.jsValue()
+    element("combo-count-input")?.max = combInput.count.jsValue()
+}
+
 
 let updateCombinationCount = JSClosure { _ in
-    element("combo-output")?.innerHTML = combinations(element("combo-count-input")?.value)
-}
-let updateCombinationInput = JSClosure { _ in
-    cInput = Array(from: element("combo-array-input")?.value.string ?? "error")
+    combCount = Int(element("combo-count-input")?.value.string ?? "1") ?? 1
 }
 
-element("combo-count-input")?.onchange = .function(updateCombinationCount)
+let updateCombinationInput = JSClosure { _ in
+    combInput = Array(from: element("combo-array-input")?.value.string ?? "")
+}
+
 element("combo-array-input")?.onchange = .function(updateCombinationInput)
+element("combo-count-input")?.onchange = .function(updateCombinationCount)
+renderCombinations()
 
 // permutations
-var pInput = [1, 2, 3, 4]
-
-let permutations = { (value: JSValue?) -> JSValue in
-    guard let value = value, let stringValue = value.string, let count = Int(stringValue) else {
-        return "Error parsing input".jsValue()
-    }
-    return pInput.permutations(ofCount: count).map { $0.description }.joined(separator: "<br>").jsValue()
+var permInput = [1, 2, 3, 4] {
+    didSet { renderPermutations() }
+}
+var permCount = 1 {
+    didSet { renderPermutations() }
 }
 
-element("permute-array-input")?.value = pInput.description.jsValue()
-element("permute-count-input")?.value = 1.jsValue()
-element("permute-output")?.innerHTML = permutations(element("permute-count-input")?.value)
+func renderPermutations() {
+    element("permute-output")?.innerHTML = permInput
+        .permutations(ofCount: permCount)
+        .map { $0.description }
+        .joined(separator: "<br>")
+        .jsValue()
+    element("permute-array-input")?.value = permInput.description.jsValue()
+    element("permute-count-input")?.value = permCount.jsValue()
+    element("permute-count-input")?.max = permInput.count.jsValue()
+}
 
 let updatePermuteCount = JSClosure { _ in
-    element("permute-output")?.innerHTML = combinations(element("permute-count-input")?.value)
+    permCount = Int(element("permute-count-input")?.value.string ?? "1") ?? 1
 }
 
 let updatePermuteInput = JSClosure { _ in
-    pInput = Array(from: element("permute-array-input")?.value.string ?? "")
+    permInput = Array(from: element("permute-array-input")?.value.string ?? "")
 }
 
 element("permute-array-input")?.onchange = .function(updatePermuteInput)
 element("permute-count-input")?.onchange = .function(updatePermuteCount)
+renderPermutations()
