9177d7e491518940c7e2b4583a3213268a76a08d
John Garrett <jacgarrett18@gmail.com>
2020-11-08 12:13:31 -0500

    add analytic and opengraph stuff to head

--- Sources/swal-rawhtml/StaticRenderer.swift
+++ Sources/swal-rawhtml/StaticRenderer.swift
@@ -29,20 +29,35 @@ struct StaticRenderer {
             <link href="https://fonts.googleapis.com/css2?family=Trispace:wght@400;500;700&display=swap" rel="stylesheet">
         """
          let analytics: String = """
-        <!-- Analytical info, you can disable this if you'd like -->
-        <script type="text/javascript">
-        var _paq = window._paq = window._paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function() {
-            var u="https://garrepidev.matomo.cloud/";
-            _paq.push(['setTrackerUrl', u+'matomo.php']);
-            _paq.push(['setSiteId', '1']);
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/garrepidev.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-        })();
-        </script>
+            <title>Swift, WebAssembly, and Algorithms</title>
+            <meta property="description" content="A sandox to explore Swift Algorithms, from the browser, using WASM">
+            <meta property="og:url" content="https://garrepi.dev/swal">
+            <meta property="og:type" content="website">
+            <meta property="og:title" content="Swift, WebAssembly, and Algorithms">
+            <meta property="og:description" content="A sandox to explore Swift Algorithms, from the browser, using WASM">
+            <meta property="og:image" content="https://garrepi.dev/images/prj/swal.png">
+            <meta property="og:image:secure_url" content="https://garrepi.dev/images/prj/swal.png">
+            <!-- Twitter Meta Tags -->
+            <meta name="twitter:card" content="summary_large_image">
+            <meta property="twitter:domain" content="garrepi.dev">
+            <meta property="twitter:url" content="https://garrepi.dev/swal">
+            <meta name="twitter:title" content="Swift, WebAssembly, and  Algorithms">
+            <meta name="twitter:description" content="A sandox to explore Swift Algorithms, from the browser, using WASM">
+            <meta name="twitter:image" content="https://garrepi.dev/images/prj/swal.png">
+            <meta property="og:locale" content="en_US">
+            <meta property="og:type" content="website">
+            <meta property="og:site_name" content="garrepidev">
+            <link rel="icon" type="image/jpeg" href="/images/prj/swal.png">
+            <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=3.0, minimum-scale=0.86">
+            <!-- I know, I hate google anayltics. i'm looking for free alternatives - you can disable this if you'd like -->
+            <script async="" src="https://www.googletagmanager.com/gtag/js?id=G-YZD103LZ87"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-YZD103LZ87');
+            </script>
         """
         
         return """
